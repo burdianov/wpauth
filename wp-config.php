0c3655432c306757facb7c7c42b7d92932054336
Lilian Burdianov <lilian.burdianov@gmail.com>
2017-09-27 23:25:54 +0400

    JWT Settings

--- wp-config.php
+++ wp-config.php
@@ -55,6 +55,9 @@ define('SECURE_AUTH_SALT', 'put your unique phrase here');
 define('LOGGED_IN_SALT',   'put your unique phrase here');
 define('NONCE_SALT',       'put your unique phrase here');
 
+/* JWT Settings */
+define('JWT_AUTH_SECRET_KEY', 'Mjvi<g-5,WB|H|K}U=n,$PuQjgX:2G&8:$d+:Ibvyb>6A(yQ}m -u`!JdIZ2+/AH');
+define('JWT_AUTH_CORS_ENABLE', true);
 /**#@-*/
 
 /**
